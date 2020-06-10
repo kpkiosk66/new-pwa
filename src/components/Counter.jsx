490e9742e3b59f4b8549f10e4dd0a090e1527992
Titivat <titivatmo@gmail.com>
2020-06-10 10:55:54 +0700

    add image

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,5 +1,6 @@
 import React , { Component } from 'react';
 import "../App.css";
+import troll from "../image/troll.jpg";
 class Counter extends Component {
 
   state = {
@@ -22,28 +23,25 @@ class Counter extends Component {
 
       render(){
     // console.log('props', this.props )
-
     return (
       <div class="col-md-6" >
-        <div class="" onClick={() => alert("Hello from here")}>
-            <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+      <div class="" onClick={() => alert("Hello from here")}>
+          <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
               <div class="col-auto d-lg-block">
-                <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+                  <img style = { {width: 200, height: 200} } src={require('../image/troll.jpg')} />
               </div>
-              <div class="col p-4 d-flex flex-column position-static">
-                <strong class="d-inline-block mb-2 text-primary">World</strong>
-                <h3 class="mb-0">Featured post</h3>
-                <div class="mb-1 text-muted">Nov 12</div>
-                <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="stretched-link">Continue reading</a>
-              </div>
+            <div class="col p-4 d-flex flex-column position-static">
+              <strong class="d-inline-block mb-2 text-primary">World</strong>
+              <h3 class="mb-0">Featured post</h3>
+              <div class="mb-1 text-muted">Nov 12</div>
+              <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+              <a href="#" class="stretched-link">Continue reading</a>
             </div>
-        </div>
-    </div>
+          </div>
+      </div>
+  </div>
     );
   }
-
-
 
   renderTags(){
     //{this.state.tags.length === 0 && "Please create a new tag!"}
